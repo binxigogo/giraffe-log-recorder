de7f2fd8d67cf3925c572f8a0236523fc426255e
wangguobin <wangguobin@wangguobin>
2019-12-01 16:05:19 +0800

    修改日志获取方式，采用LogAdaptManager获取日志

--- src/main/java/pre/binxigogo/log/LogAspect.java
+++ src/main/java/pre/binxigogo/log/LogAspect.java
@@ -5,7 +5,9 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -13,6 +15,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import pre.binxigogo.log.adapter.LogAdaptManager;
 import pre.binxigogo.log.annotation.IgnoreParam;
 import pre.binxigogo.log.annotation.Log;
 import pre.binxigogo.log.annotation.LogPoint;
@@ -21,12 +24,13 @@ import pre.binxigogo.log.format.LogMessageFormatter;
 
 @Aspect
 public class LogAspect {
-	private Logger logger;
+	private static final Map<String, Logger> classLoggers = new ConcurrentHashMap<>();
+	private LogAdaptManager logAdaptManager;
 	private LogMessageFormatter logMessageFormatter;
 
-	public LogAspect(Logger logger, LogMessageFormatter logMessageFormatter) {
-		this.logger = logger;
+	public LogAspect(LogAdaptManager logAdaptManager, LogMessageFormatter logMessageFormatter) {
 		this.logMessageFormatter = logMessageFormatter;
+		this.logAdaptManager = logAdaptManager;
 	}
 
 	@Pointcut(value = "@annotation(pre.binxigogo.log.annotation.Log)")
@@ -41,18 +45,19 @@ public class LogAspect {
 		Object[] args = joinPoint.getArgs();
 		String[] parameterNames = signature.getParameterNames();
 		String className = joinPoint.getTarget().getClass().getName();
+		Logger logger = getLogger(className);
 		Set<Integer> ignoreMethodParameterPos = getIgnoreMethodParameterPos(method);
 		String methodName = method.getName();
 		// 方法执行之前记录日志
-		beforeLog(log, className, methodName, parameterNames, args, ignoreMethodParameterPos);
+		beforeLog(log, logger, className, methodName, parameterNames, args, ignoreMethodParameterPos);
 		Object result = null;
 		try {
 			result = joinPoint.proceed();
 			// 方法执行之后记录日志
-			afterLog(log, className, methodName, parameterNames, args, result, ignoreMethodParameterPos);
+			afterLog(log, logger, className, methodName, parameterNames, args, result, ignoreMethodParameterPos);
 		} catch (Throwable t) {
 			// 方法抛出异常记录日志
-			exceptionLog(log, className, methodName, parameterNames, args, t, ignoreMethodParameterPos);
+			exceptionLog(log, logger, className, methodName, parameterNames, args, t, ignoreMethodParameterPos);
 			throw t;
 		}
 		return result;
@@ -70,6 +75,15 @@ public class LogAspect {
 		return ignoreMethodParameterPos;
 	}
 
+	private Logger getLogger(String className) {
+		Logger logger = classLoggers.get(className);
+		if (logger == null) {
+			logger = logAdaptManager.getLogger(className);
+			classLoggers.put(className, logger);
+		}
+		return logger;
+	}
+
 	private boolean isContainAnnotation(Annotation[] annotations, Class<?> existAnnotation) {
 		for (Annotation annotation : annotations) {
 			if (annotation.annotationType().getName().equals(existAnnotation.getName())) {
@@ -79,34 +93,35 @@ public class LogAspect {
 		return false;
 	}
 
-	private void beforeLog(Log log, String className, String methodName, String[] parameterNames, Object[] args,
-			Set<Integer> ignoreMethodParameterPos) {
-		log(LogPointPosition.BEFORE, log, className, methodName, parameterNames, args, null, null,
+	private void beforeLog(Log log, Logger logger, String className, String methodName, String[] parameterNames,
+			Object[] args, Set<Integer> ignoreMethodParameterPos) {
+		log(LogPointPosition.BEFORE, log, logger, className, methodName, parameterNames, args, null, null,
 				ignoreMethodParameterPos);
 	}
 
-	private void afterLog(Log log, String className, String methodName, String[] parameterNames, Object[] args,
-			Object result, Set<Integer> ignoreMethodParameterPos) {
-		log(LogPointPosition.AFTER, log, className, methodName, parameterNames, args, result, null,
+	private void afterLog(Log log, Logger logger, String className, String methodName, String[] parameterNames,
+			Object[] args, Object result, Set<Integer> ignoreMethodParameterPos) {
+		log(LogPointPosition.AFTER, log, logger, className, methodName, parameterNames, args, result, null,
 				ignoreMethodParameterPos);
 	}
 
-	private void exceptionLog(Log log, String className, String methodName, String[] parameterNames, Object[] args,
-			Throwable t, Set<Integer> ignoreMethodParameterPos) {
-		log(LogPointPosition.EXCEPTION, log, className, methodName, parameterNames, args, null, t,
+	private void exceptionLog(Log log, Logger logger, String className, String methodName, String[] parameterNames,
+			Object[] args, Throwable t, Set<Integer> ignoreMethodParameterPos) {
+		log(LogPointPosition.EXCEPTION, log, logger, className, methodName, parameterNames, args, null, t,
 				ignoreMethodParameterPos);
 	}
 
-	private void log(LogPointPosition logPointPosition, Log log, String className, String methodName,
+	private void log(LogPointPosition logPointPosition, Log log, Logger logger, String className, String methodName,
 			String[] parameterNames, Object[] args, Object result, Throwable t, Set<Integer> ignoreMethodParameterPos) {
 		LogPoint logPoint = containPoint(log.points(), logPointPosition);
 		if (logPoint != null) {
-			format(log, logPoint, className, methodName, parameterNames, args, result, t, ignoreMethodParameterPos);
+			format(log, logger, logPoint, className, methodName, parameterNames, args, result, t,
+					ignoreMethodParameterPos);
 		}
 	}
 
-	private void format(Log log, LogPoint logPoint, String className, String methodName, String[] parameterNames,
-			Object[] args, Object result, Throwable t, Set<Integer> ignoreMethodParameterPos) {
+	private void format(Log log, Logger logger, LogPoint logPoint, String className, String methodName,
+			String[] parameterNames, Object[] args, Object result, Throwable t, Set<Integer> ignoreMethodParameterPos) {
 		// 如果有需要忽略的方法参数，重新组装参数名称和参数值数组
 		if (!ignoreMethodParameterPos.isEmpty()) {
 			int noneIgnoreParamNum = parameterNames.length - ignoreMethodParameterPos.size();
